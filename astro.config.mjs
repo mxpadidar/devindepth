2ab79aac3b0f4873b0624b78bf56b9144b634a06
mxpadidar <mxpadidar@gmail.com>
2025-12-27 18:22:14 +0330

    base layout and require components and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  markdown: {
+    shikiConfig: {
+      theme: "ayu-dark",
+    },
+  },
+
+  integrations: [icon()],
 });
